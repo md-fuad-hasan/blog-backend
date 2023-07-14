38b19c0b9943ba61f26d17698e064394102989a5
Fuad Hasan <fuad.ice.ru@gmail.com>
2023-07-15 00:21:42 +0600

    Custom User Model using email and Username

--- Blog_Backend/Blog_Backend/settings.py
+++ Blog_Backend/Blog_Backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account',
+    'blog',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,8 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTH_USER_MODEL = 'account.Account'
 
 WSGI_APPLICATION = 'Blog_Backend.wsgi.application'
 
---
+++ Blog_Backend/account/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from account.models import Account
+# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ("email", "username", "is_active", "is_staff")
+    search_fields = ('email','username')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account,AccountAdmin)
